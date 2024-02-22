4374c08e416d865fa9390165c31ae680cce35731
Preston Guillory <pguillory@gmail.com>
2024-02-21 22:06:30 -0800

    Add package to mix.exs

--- mix.exs
+++ mix.exs
@@ -3,11 +3,23 @@ defmodule Superlist.MixProject do
 
   def project do
     [
+      package: package()
       app: :superlist,
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      description: "Optimized operations on multiple lists",
+      licenses: ["MIT"],
+      maintainers: ["pguillory@gmail.com"],
+      links: %{
+        "GitHub" => "https://github.com/pguillory/superlist"
+      }
     ]
   end
 
