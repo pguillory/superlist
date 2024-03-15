64cf996cce1a4be6710cf7f7380b04cb7444c925
Preston Guillory <pguillory@gmail.com>
2024-03-15 08:25:30 -0700

    Add more tests for transpose

--- test/superlist_test.exs
+++ test/superlist_test.exs
@@ -33,6 +33,8 @@ defmodule SuperListTest do
   end
 
   test "transpose" do
+    assert transpose([[1], [2]]) == [[1, 2]]
+    assert transpose([[1, 2]]) == [[1], [2]]
     assert transpose([[1, 3], [2, 4]]) == [[1, 2], [3, 4]]
   end
 
