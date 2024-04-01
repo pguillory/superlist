d588c7edfd35789da1c675c472e046dffc0fd2ca
Preston Guillory <pguillory@gmail.com>
2024-04-01 12:50:57 -0700

    Bump version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Superlist.MixProject do
     [
       package: package(),
       app: :superlist,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
